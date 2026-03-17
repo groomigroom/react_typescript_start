663eefc83f7bc7459819ae14f72e9ca011485b03
groomigroom <googjohn0143@gmail.com>
2026-03-17 06:11:28 +0000

    makerandomdom

--- src/data/date.ts
+++ src/data/date.ts
@@ -10,3 +10,6 @@ export const makeRelativeDate = (date: Date) =>
     DateTime.fromJSDate(date).startOf('day').toRelative();
 export const randomRelativeDate = () => makeRelativeDate(makeRandomPastDate());
 
+export const makeDayMonthYear = (date: Date) =>
+    DateTime.fromJSDate(date).toLocaleString(DateTime.DATE_FULL);
+export const randomDayMonthYear = () => makeDayMonthYear(makeRandomPastDate());
