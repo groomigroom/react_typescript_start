e3ad914803e59178d62cad6aa62eec922390a908
groomigroom <googjohn0143@gmail.com>
2026-03-17 06:08:51 +0000

    makerandompastdate

--- src/data/date.ts
+++ src/data/date.ts
@@ -1,8 +1,12 @@
 import {DateTime} from 'luxon';
 
-export const mkaeRandomPastDate = () => {
+export const makeRandomPastDate = () => {
     const value = new Date().valueOf();
     const n = 100000;
     return new Date(value - Math.floor(Math.random() * n * n));
 }
+
+export const makeRelativeDate = (date: Date) =>
+    DateTime.fromJSDate(date).startOf('day').toRelative();
+export const randomRelativeDate = () => makeRelativeDate(makeRandomPastDate());
 
