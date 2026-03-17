6b1c5dd882c723961d65c50a8d4bb794c6d40e34
groomigroom <googjohn0143@gmail.com>
2026-03-17 05:42:54 +0000

    sizesize

--- src/data/image.ts
+++ src/data/image.ts
@@ -6,4 +6,9 @@ export const randomImage = (
     w: number = 1000,
     h: number = 800,
     delta: number = 200
-): string => picsumUrl(U.random(w, w + delta), U.random(h, h + delta)) 
+): string => picsumUrl(U.random(w, w + delta), U.random(h, h + delta))
+
+export const randomAvatar = () => {
+    const size = U.random(200, 400)
+    return picsumUrl(size, size)
+}
