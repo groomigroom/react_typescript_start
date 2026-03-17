1cbc45f9fd6e5fce01e0e7e5800b164e8376f855
groomigroom <googjohn0143@gmail.com>
2026-03-17 06:16:02 +0000

    randomdomdom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import * as D from './data';
+
 import './App.css';
 
-function App() {
-  console.log("groom");
-  return (
-    <h1>hello worldworld</h1>
-  );
-}
+// function App() {
+//   console.log("groom");
+//   return (
+//     <h1>hello worldworld</h1>
+//   );
+// }
 
-export default App;
+// export default App;
+
+export default function App(){
+  return (
+    <div>
+      <p>
+        {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+      </p>
+    </div>
+  )
+}
