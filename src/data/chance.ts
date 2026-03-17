c0509df7d01b293708ed74759355fcc506804ed7
groomigroom <googjohn0143@gmail.com>
2026-03-17 06:01:56 +0000

    chancetsts

--- src/data/chance.ts
+++ src/data/chance.ts
@@ -8,5 +8,7 @@ export const randomId = () => chance.fbid();
 export const randomJobTitle = () => chance.profession();
 export const randomCompanyName = () => chance.company();
 export const randomSentence = (words = 5) => chance.sentence({words});
+export const randomTitleText = (words = 3) => chance.sentence({words});
+export const randomParagraphs = (sentences = 3) => chance.paragraph({sentences});
 
 
